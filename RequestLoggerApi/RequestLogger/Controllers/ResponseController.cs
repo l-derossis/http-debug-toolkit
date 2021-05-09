39e070aae8c04c31771d43373920e7cf31e677c7
lderossis <lderossis@witekio.com>
2021-05-09 16:06:14 +0200

    Removed useless usings

--- RequestLoggerApi/RequestLogger/Controllers/ResponseController.cs
+++ RequestLoggerApi/RequestLogger/Controllers/ResponseController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using RequestLogger.Domain.Entities;
 using RequestLogger.Domain.Services;
 using RequestLogger.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace RequestLogger.Controllers
 {
