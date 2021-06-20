34a6f1985c331922efb5b76a9ea31f955f59193a
lderossis <leoderossis@yahoo.fr>
2021-06-20 23:38:37 +0200

    Fixed failing unit test

--- RequestLoggerSpa/request-logger/src/app/components/endpoint-update/endpoint-update.component.spec.ts
+++ RequestLoggerSpa/request-logger/src/app/components/endpoint-update/endpoint-update.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EndpointsApiService } from 'src/app/services/endpoints-api.service';
 
 import { EndpointUpdateComponent } from './endpoint-update.component';
 
 describe('EndpointUpdateComponent', () => {
   let component: EndpointUpdateComponent;
   let fixture: ComponentFixture<EndpointUpdateComponent>;
+  const serviceStub: Partial<EndpointsApiService> = {};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EndpointUpdateComponent ]
-    })
-    .compileComponents();
+      declarations: [EndpointUpdateComponent],
+      providers: [{ provide: EndpointsApiService, useValue: serviceStub }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
